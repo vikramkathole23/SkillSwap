00fc64e6a3cd9328ebd61e23226bfd6a35c25ccd
vikram kathole <vikramkathol225@gmail.com>
2026-01-09 17:53:56 +0530

    fix: update sender email address in mail options

--- backend/controllers/sendMail.controler.js
+++ backend/controllers/sendMail.controler.js
@@ -29,7 +29,7 @@ const sendMail = async (toMail, subject, code, userName) => {
 
 
     let mailOptions = {
-      from: "SkillSwap <noreply@skillswap.com>",
+      from: "SkillSwap <teamskillswap12@gmail.com>",
       to: toMail,
       subject: subject,
       html,
