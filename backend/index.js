d1575258975f98bd7672cf06efa653bd2cd8ff52
vikram kathole <vikramkathol225@gmail.com>
2026-01-16 16:59:23 +0530

    fix: fix cors

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,8 @@ const server = createServer(app);
 const allowedOrigins = process.env.Frontend_URL;
 const corsOption = {
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
+     if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
