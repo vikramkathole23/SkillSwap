a1a45cffe2b609ffd94d99d1f2d99dde0dca06a3
vikram kathole <vikramkathol225@gmail.com>
2026-02-17 12:20:50 +0530

    fix: update production flag to enable production environment

--- backend/production.js
+++ backend/production.js
@@ -1,4 +1,4 @@
-let Is_prod = false;
+let Is_prod = true;
 // const Frontend_URL = "https://skill-swap-lilac-gamma.vercel.app"||"http://localhost:5173"
 const Frontend_URL = Is_prod ? "https://skill-swap-lilac-gamma.vercel.app":"http://localhost:5173"
 
